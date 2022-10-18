fbc48e0029ed6cd0e0c002546d87adf4c305752a
shivam chourasiya <shivu2022.cotocus@gmail.com>
2022-10-18 19:26:14 +0530

    external_php

---
+++ external.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+			<h1> external way </h1><br><br>
+			
+			<h3 style = color:green;> head tag</h3>
+			
+			<script src="index.js" type="text/javascript">
+			
+			</script>
+			
+	</head>
+	
+	   <body>
+	
+			<h3 style = color:green;> body tag</h3>
+			
+		
+			<script src="index.js" type="text/javascript">
+			
+			</script>
+			
+	   </body>
+</html>
---
+++ inline.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+			<title> Inline way to use </title>
+			
+			<h3 style = color:red;> head tag</h3>
+			<!-- this is the inline way to use javascript head tag.-->
+			<script type="text/javascript">
+			
+			document.write("  adding javascript in the head tag ")
+			
+			</script>
+			
+	</head>
+	
+	<body>
+	
+			<h3 style = color:red;> body tag </h3>
+			
+			<p> In inline way we have two option <br> inside head tag<br> inside body tag <br> </p>
+			
+			<h2 style = color:blue;> this is the way to adding javascript in body tag</h2>
+			
+	</body>
+</html>
