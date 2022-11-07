3790bb8f5ebb958ec29dd300b3fd427b59257b71
shivam chourasiya <shivu2022.cotocus@gmail.com>
2022-11-07 20:28:33 +0530

    push_php

---
+++ push.php
@@ -0,0 +1,27 @@
+<?php
+
+		$color=array("red","green");
+		
+		array_push($color,"blue","yellow");
+		
+		  echo "<pre>";
+		  
+		print_r($color);
+		
+		 echo "</pre>";
+		 
+		 
+		 echo "<br>";
+		 echo "<br>";
+		 
+		
+		$color=array("red","green","yellow","blue");
+		
+		array_pop($color);
+		
+		  echo "<pre>";
+		  
+		print_r($color);
+		
+		 echo "</pre>"; 
+?>
